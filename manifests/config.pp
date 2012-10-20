f6fc21bc5cdb95e03454ff689512802dc40a9d19
lbennett <lbennett@mimecast.com>
2012-10-20 20:47:55 +0100

    adding host var

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,7 +5,8 @@ class sickbeard::config {
     $newzbin_username = extlookup("newzbin_username")
     $newzbin_password = extlookup("newzbin_password")
     $sab_host = extlookup("sab_host")
-    $sab_apikey = extlookup("sab_apikey") 
+    $sab_apikey = extlookup("sab_apikey")
+    $sickbeard_host = extlookup("sickbeard_host")
     $sickbeard_port = extlookup("sickbeard_port")
     
     $nzb_dir = "/usr/local/SABnzbd-downloads/listen/TV"
